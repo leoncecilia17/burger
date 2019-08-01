25f6d7a5f3a6ff3fb6bdfbb27a5c769be3b5a11d
Cecilia Leon <leoncecilia17@gmail.com>
2019-08-01 13:30:38 -0500

    set up orm

--- config/connection.js
+++ config/connection.js
@@ -1,2 +1,27 @@
-// set up the code to connect Node to MySQL 
-// export the connection 
+var mysql = require("mysql");
+
+var connection;
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "cecilia",
+        password: "Sagelove69!17",
+        database: "burgers_db"
+    });
+}
+
+connection.connect(function (error) {
+    if (error) {
+        console.log("Error connecting: " + error.stack);
+        return;
+    }
+    console.log("Connected as id " + connection.threadId);
+});
+
+module.exports = connection;
+
--- config/orm.js
+++ config/orm.js
@@ -1,9 +1,70 @@
-// import require connection.js into orm.js 
+var connection = require("../config/connection.js"); 
 
-// create the methods that will execute the necessary MySQL commands in the controllers. These are the methods you will need to use in order to retrieve and store data in your database. 
+function printQuestionMarks(num){
+    var arr = []; 
+    for (var i = 0; i < num; i++){
+        arr.push("?")
+    }
+    return arr.toString(); 
+}
 
-// selectAll()
-// insertOne()
-// updateOne()
+function objToSql(ob){
+    var arr = []; 
+    for (var key in ob){
+        var value = ob[key]; 
+        if (Object.hasOwnProperty.call(ob, key)){
+            if (typeof value === "string" && value.indexOf(" ") >= 0){
+                value = " ' " + value + " ' "; 
+            }
+        arr.push(key + " = " + value); 
+        }
+    }
+    return arr.toString(); 
+}
 
-// export the ORM object in module.exports 
+var orm = {
+    selectAll: function(burgerInput, cb){
+        var queryString = "SELECT * FROM" + burgerInput + ";"; 
+        connection.query(queryString, function(err, result){
+            if (err){
+                throw err; 
+            }
+            cb(result); 
+        }); 
+    }, 
+    insertOne: function(table, cols, vals, cb){
+        var queryString = "INSERT INTO " + table; 
+        queryString += " ("; 
+        queryString += cols.toString(); 
+        queryString += ") "
+        queryString += "VALUES ("; 
+        queryString += printQuestionMarks(vals.length); 
+        queryString += ") "; 
+
+        console.log(queryString); 
+
+        connection.query(queryString, val, function(err, result){
+            if(err) {
+                throw err; 
+            }
+        cb(result); 
+        }); 
+    }, 
+    updateOne: function(table, objColCals, condition, cb){
+        var queryString = "UPDATE " + table; 
+        queryString += "SET"; 
+        queryString += objToSql(objColVals); 
+        queryString += "WHERE"; 
+        queryString += condition; 
+        
+        console.log(queryString); 
+        connection.query(queryString, function(err, result){
+            if (err){
+                throw err; 
+            }
+            cb(result); 
+        }); 
+    }, 
+}; 
+
+module.exports = orm; 
