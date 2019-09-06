5cafb17a7bf4c74be96b300f045bdeed767690ba
Cecilia Leon <leoncecilia17@gmail.com>
2019-09-05 22:45:36 -0500

    set up post request for burgers

--- public/assets/js/burger.js
+++ public/assets/js/burger.js
@@ -1,16 +1,33 @@
-$(function() {
-    $(".changetoDevour").on("click", function() {
-      var id = $(this).data("id");
-      // Send the PUT request.
-      $.ajax("/api/burgers/" + id, {
-        type: "PUT"
-      }).then(
-        function(response) {
-          console.log("changed burger to true");
-          // Reload the page to get the updated list
-          location.reload();
-        }
-      )
-    });
+$(function () {
+  $(".changetoDevour").on("click", function () {
+    var id = $(this).data("id");
+    // Send the PUT request.
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT"
+    }).then(
+      function (response) {
+        console.log("changed burger to true");
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
 
+  $(".create-burger").on("submit", function (event) {
+    event.preventDefault();
+
+    var newBurger = {
+      name: $("#bu").val().trim(),
+    }; 
+
+    $.ajax("/api/burgers", {
+      type: "POST", 
+      data: newBurger
+    }).then(
+      function(){
+        console.log("created a new burger"); 
+        location.reload(); 
+      }
+    ); 
+  }); 
 });
